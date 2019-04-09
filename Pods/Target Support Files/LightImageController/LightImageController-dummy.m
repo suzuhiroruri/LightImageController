5edf896c8c0fe1e15afc4f1658036e3a2b5bac7b
suzuhiroruri <suzuhiroururi@gmail.com>
2019-04-09 16:16:18 +0900

    cocoaPods対応

---
+++ Pods/Target Support Files/LightImageController/LightImageController-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LightImageController : NSObject
+@end
+@implementation PodsDummy_LightImageController
+@end
---
+++ Pods/Target Support Files/Pods-LightImageController_Tests/Pods-LightImageController_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LightImageController_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_LightImageController_Tests
+@end
